403f18a9f0caa94339cfc64829f5473dcb253d6a
pawel <pawli444@gmail.com>
2024-12-17 23:22:30 +0100

    html semantics + component for two sites

--- src/pages/partnersPage.tsx
+++ src/pages/partnersPage.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { MOCK_COMPANIES } from '@/mocks/companies';
+import PartnersCompanies from '@/components/pages/landing/partnersCompanies/partnersCompanies';
 
 const partnersPage = () => {
   return (
-    <div>partnersPage</div>
+    <PartnersCompanies companies={MOCK_COMPANIES} title="PARTNERZY" />
   )
 }
 
