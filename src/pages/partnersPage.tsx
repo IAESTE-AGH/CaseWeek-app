4fb06cf6748e19cb56dc157e5edb0ff335d8db32
pawel <pawli444@gmail.com>
2024-12-17 19:52:40 +0100

    add partners logos

--- src/pages/partnersPage.tsx
+++ src/pages/partnersPage.tsx
@@ -1,7 +1,33 @@
-const partnersPage = () => {
-  return (
-    <div>partnersPage</div>
-  )
-}
+import React from 'react'
+//  import { useState, useEffect } from 'react';
+import { useParams, useLoaderData, useNavigate } from 'react-router-dom';
+//import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import './partnersPage.css'
+import naTeraz from '../assets/img/temp.png';
 
-export default partnersPage;
+import { MOCK_COMPANIES } from '../mocks/companies';
+
+const PartnersPage: React.FC = () => {
+  return (
+    <div>
+      <h1 className="title">PARTNERZY:</h1>
+      <div className="partners-grid">
+        {MOCK_COMPANIES.map((company) => (
+          <div key={company.id} className="partner">
+            <a href={company.websiteUrl} target="_blank" rel="noopener noreferrer">
+              <img src={naTeraz} alt={company.name} />
+            </a>
+            <p className="partner-name">{company.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+export default PartnersPage;
+
+
